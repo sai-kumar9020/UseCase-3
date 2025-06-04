3efc071fa4bf5547fab67373ee2f7d9451835c20
sai-kumar9020 <100404258+sai-kumar9020@users.noreply.github.com>
2025-06-04 10:48:46 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "app_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public.id]
+  subnets            = [aws_subnet.public_subnet_1.id , aws_subnet.public_subnet_2.id]
 }
 
 resource "aws_lb_target_group" "tg" {
