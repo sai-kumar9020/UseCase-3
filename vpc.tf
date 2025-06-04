6bdb365ac2da894eb43e873687ca27a72dc85928
sai-kumar9020 <100404258+sai-kumar9020@users.noreply.github.com>
2025-06-04 10:42:30 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -41,6 +41,11 @@ resource "aws_route" "default_route" {
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
 }
